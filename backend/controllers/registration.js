a1b24d1347bdc0a105525fb101dc747caa6fec85
Chetan Pareek <pareekchetan7891@gmail.com>
2021-10-10 12:07:22 +0530

    login added

--- backend/controllers/registration.js
+++ backend/controllers/registration.js
@@ -1,6 +1,7 @@
 const teacher = require("./../models/teacher_data");
 const student = require("./../models/student_data");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { customAlphabet } = require("nanoid");
 
 exports.registration = async (req, res) => {
@@ -39,12 +40,110 @@ exports.student_registration = async (req, res) => {
       student_id: nanoid(),
       password: hashedPassword,
     });
-    const teacher_data = await data.save();
+    const _data = await data.save();
     res.status(201).json({
       status: "success",
       message: `teacher created with username ${req.query.username}`,
     });
   } catch (error) {
     res.send(error);
+  }
+};
+
+exports.teacher_login = async (req, res, next) => {
+  const email = req.query.email;
+  if (!email) {
+    return res.json({
+      status: "failure",
+      message: `please enter email`,
+    });
+  }
+  const pswd = req.query.password;
+  if (!pswd) {
+    return res.json({
+      status: "failure",
+      message: `please enter password`,
+    });
+  }
+  var data = await teacher.find({ email: email });
+  if (data.length == 0) {
+    return res.json({
+      status: "failure",
+      message: `${email} not found, please register`,
+    });
+  }
+
+  var correct_password = await bcrypt.compare(pswd, data[0].password);
+  if (correct_password) {
+    var token = jwt.sign(
+      {
+        username: data[0].username,
+        email: data[0].email,
+        teacher_id: data[0].teacher_id,
+      },
+      "qwertop098",
+      { expiresIn: "4h" }
+    );
+    var decoded_values = jwt.decode(token, "qwertop098");
+    return res.json({
+      status: "success",
+      message: "login successful",
+      token,
+      decoded_values,
+    });
+  } else {
+    return res.json({
+      status: "failure",
+      message: `invalid password, please try again`,
+    });
+  }
+};
+
+exports.student_login = async (req, res, next) => {
+  const email = req.query.email;
+  if (!email) {
+    return res.json({
+      status: "failure",
+      message: `please enter email`,
+    });
+  }
+  const pswd = req.query.password;
+  if (!pswd) {
+    return res.json({
+      status: "failure",
+      message: `please enter password`,
+    });
+  }
+  var data = await student.find({ email: email });
+  if (data.length == 0) {
+    return res.json({
+      status: "failure",
+      message: `${email} not found, please register`,
+    });
+  }
+
+  var correct_password = await bcrypt.compare(pswd, data[0].password);
+  if (correct_password) {
+    var token = jwt.sign(
+      {
+        username: data[0].username,
+        email: data[0].email,
+        teacher_id: data[0].teacher_id,
+      },
+      "qwertop098",
+      { expiresIn: "4h" }
+    );
+    var decoded_values = jwt.decode(token, "qwertop098");
+    return res.json({
+      status: "success",
+      message: "login successful",
+      token,
+      decoded_values,
+    });
+  } else {
+    return res.json({
+      status: "failure",
+      message: `invalid password, please try again`,
+    });
   }
 };
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,12 +1,9 @@
 const app = require("express")();
 const registration = require("./../controllers/registration");
 
-//uploader-APIs
-
-//teacher-APIs
 app.route("/teacher/register").post(registration.registration);
 app.route("/student/register").post(registration.student_registration);
-
-//student-APIs
+app.route("/teacher/login").post(registration.teacher_login);
+app.route("/student/login").post(registration.student_login);
 
 module.exports = app;
